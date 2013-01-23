d776e6f13617c18b26ffce97aa2e16a85bd33bff
David Malcolm <dmalcolm@redhat.com>
2013-01-22 20:47:00 -0500

    add setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='gccinvocation',
+    version='0.0.1',
+    description='Library for parsing GCC command-line options',
+    py_modules = ['gccinvocation'],
+    license='GPL3',
+    author='David Malcolm <dmalcolm@redhat.com>',
+    url='https://github.com/fedora-static-analysis/gccinvocation',
+    classifiers=(
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries',
+    )
+)
